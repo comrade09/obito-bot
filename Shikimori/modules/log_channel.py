2c078dc3e37917eac4d71be2e3fec6168e146746
comet44 <111640636+comet44@users.noreply.github.com>
2022-12-17 15:28:42 +0530

    Update log_channel.py

--- Shikimori/modules/log_channel.py
+++ Shikimori/modules/log_channel.py
@@ -229,7 +229,7 @@ if is_module_loaded(FILENAME):
 ➩ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "Log Channel 📢​"
+    __mod_name__ = "Log Channel"
 
     LOG_HANDLER = CommandHandler("logchannel", logging, run_async=True)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog, run_async=True)
