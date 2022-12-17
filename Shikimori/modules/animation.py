5924d53d8222e5594670b2e76dd2448abca70c97
comet44 <111640636+comet44@users.noreply.github.com>
2022-12-17 14:54:54 +0530

    Update animation.py

--- Shikimori/modules/animation.py
+++ Shikimori/modules/animation.py
@@ -132,12 +132,12 @@ love_siren = [
 
 
 hack_you = [
-    "Looking for WhatsApp databases in targeted person...",
+    "Looking for Telegram Account Details on Dark Web of this targeted account...",
     " User online: True\nTelegram access: True\nRead Storage: True ",
-    "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]",
-    "Hacking... 86.21%\n[███████████████░░░░░]",
-    "Hacking... 93.50%\n[█████████████████░░░]",
-    "hacking....  100%\n[████████████████████]",
+    "Hacking found telegram phone no.... 20.63%\n[███░░░░░░░░░░░░░░░░░]",
+    "Hacking otp received... 86.21%\n[███████████████░░░░░]",
+    "Hacking account accesed... 93.50%\n[█████████████████░░░]",
+    "hacking account hacked successfull....  100%\n[████████████████████]",
 ]
 
 
